58fe8118bb73778646ea3f5da2be45d6f7840cf7
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-08-21 13:13:22 +0530

    minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(),tailwindcss()],
-})
+  base: mode === 'production' ? '/Checkify.Ai/' : '/' // relative for dev, repo path for GH Pages
+}))
